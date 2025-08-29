1f744148c78361a299d7cc5bc202e31955294754
Adryano Junior <jusamsung10@gmail.com>
2025-08-29 13:14:48 -0300

    base url added

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "https://demo.sylius.com/en_US/"
+  },
+});
