ba5bd087c2242d002517c0e8433fefb6c6c77260
Adryano Junior <jusamsung10@gmail.com>
2025-09-03 12:51:20 -0300

    added custom commands to main system flows

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -17,3 +17,38 @@ import { faker } from '@faker-js/faker';
         cy.get('#register-button').click()
 
  })
+
+  Cypress.Commands.add('choosingProduct', () => {
+   cy.get('[href="/en_US/taxons/fashion-category/dresses"]').click()
+   cy.get("[product='Summer tunic']").click()
+   
+ })
+
+ Cypress.Commands.add('selectVariations', () => {
+   cy.get("[data-option='dress_size']").select('XL')
+   cy.get("[data-option='dress_height']").select('Tall')
+   cy.get("#sylius_shop_add_to_cart_cartItem_quantity").clear().type('2')
+   cy.get('#add-to-cart-button').click()
+   
+ })
+
+ Cypress.Commands.add('checkoutButton', () => {
+   cy.get("[type='submit']").click()
+   
+ })
+
+
+  Cypress.Commands.add('fillBillingAddress', () => {
+    cy.get("#sylius_shop_checkout_address_customer_email").type(faker.internet.email({ firstName: 'Scott'}))
+    cy.get("#sylius_shop_checkout_address_billingAddress_firstName").type(faker.person.firstName())
+    cy.get("#sylius_shop_checkout_address_billingAddress_lastName").type(faker.person.lastName())
+    cy.get("#sylius_shop_checkout_address_billingAddress_company").type(faker.company.name())
+    cy.get("#sylius_shop_checkout_address_billingAddress_street").type(faker.location.streetAddress())
+    cy.get("#sylius_shop_checkout_address_billingAddress_countryCode").select('United States')
+    cy.get("#sylius_shop_checkout_address_billingAddress_city").type(faker.location.city())
+    cy.get("#sylius_shop_checkout_address_billingAddress_postcode").type(faker.location.zipCode())
+    cy.get("#sylius_shop_checkout_address_billingAddress_phoneNumber").type(faker.phone.number())
+    cy.get(".btn-primary").click()
+
+ })
+
