5506ebe532d27e9ce1c7f189383a7ca2711a9ead
Adryano Junior <jusamsung10@gmail.com>
2025-09-03 12:49:55 -0300

    implementing successful login and invalid login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,5 @@
+import userData from '../fixtures/userData.json'
+
 describe('Login functionality test cases - Demo Sylius', () => {
 
   beforeEach(() => {
@@ -5,7 +7,12 @@ describe('Login functionality test cases - Demo Sylius', () => {
   });
 
   it('Login - Success', () => {
-    cy.login('fashion@example.com', 'sylius')
+    cy.login(userData.userSuccess.username, userData.userSuccess.password)
     cy.get('body').should('contain', 'My account')
   })
+
+  it('Login - Fail', () => {
+     cy.login(userData.userFail.username, userData.userFail.password)
+     cy.get('body').should('contain', 'Invalid credentials')
+  });
 })
