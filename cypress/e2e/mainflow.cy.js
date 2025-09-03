a4c20dd80bad1f58bd48e1ffcd5336c3b14abb63
Adryano Junior <jusamsung10@gmail.com>
2025-09-03 12:42:18 -0300

    refactoring code with custom commands

---
+++ cypress/e2e/mainflow.cy.js
@@ -0,0 +1,23 @@
+describe('System Main Flow - Demo Sylius', () => {
+
+    before(() => {
+        cy.visit('register')
+    });
+    
+    it.only('From registration to finalizing the order', () => {
+        cy.register()
+        cy.get('body').should('contain', 'Thank you for registering, check your email to verify your account')
+
+        cy.choosingProduct()
+        cy.get('body').should('contain', 'Details')
+
+        cy.selectVariations()
+        cy.get('body').should('contain', 'Item has been added to cart')
+        cy.get('body').should('contain', 'Edit your items, apply coupon or proceed to the checkout')
+      
+        cy.checkoutButton()
+
+        cy.fillBillingAddress()
+        cy.get('body').should('contain', 'Order total:')
+    });
+});
