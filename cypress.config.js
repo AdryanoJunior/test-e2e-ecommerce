52f5a81661758ec4da55725846bc80b9fb74b124
Adryano Junior <jusamsung10@gmail.com>
2025-09-03 12:44:33 -0300

    integrating automation with cy cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "9td5o6",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://demo.sylius.com/en_US/"
+    baseUrl: "https://demo.sylius.com/en_US/",
+    //video: true,
   },
 });
